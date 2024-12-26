d3d96c8b6525e6c5c4eb106d7c1c91f579b5369a
Tizimmi <gg_tizimmi@mail.ru>
2024-12-26 21:59:05 +0700

    goblin model import +  default movement component update for rotation

--- TowerDefence/Assets/TowerDefence/Scripts/EnemyLogic/MovementLogic/DefaultMovementComponent.cs
+++ TowerDefence/Assets/TowerDefence/Scripts/EnemyLogic/MovementLogic/DefaultMovementComponent.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using TowerDefence.Scripts.WaypointsSystem;
+﻿using TowerDefence.Scripts.WaypointsSystem;
 using UnityEngine;
 
 namespace TowerDefence.Scripts.EnemyLogic.MovementLogic
 {
 	public class DefaultMovementComponent : MovementComponent
 	{
+		[Range(1f, 10f)]
+		[SerializeField]
+		private float _rotationSpeed;
+
 		public override void Init(Waypoints path)
 		{
 			Path = path;
@@ -14,6 +17,8 @@ namespace TowerDefence.Scripts.EnemyLogic.MovementLogic
 			transform.position = CurrentWaypoint.position;
 
 			CurrentWaypoint = Path.GetNextWaypoint(CurrentWaypoint);
+			
+			transform.LookAt(CurrentWaypoint);
 		}
 
 		public override void Move(float speed)
@@ -25,9 +30,19 @@ namespace TowerDefence.Scripts.EnemyLogic.MovementLogic
 			}
 
 			transform.position = Vector3.MoveTowards(transform.position, CurrentWaypoint.position, speed * Time.deltaTime);
+			
+			RotateToWaypoint();
 
 			if (Vector3.Distance(transform.position, CurrentWaypoint.position) < _pathOffset)
 				CurrentWaypoint = Path.GetNextWaypoint(CurrentWaypoint);
+		}
+
+		private void RotateToWaypoint()
+		{
+			var direction = CurrentWaypoint.transform.position - transform.position;
+			var lookRotation = Quaternion.LookRotation(direction);
+			var rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * _rotationSpeed).eulerAngles;
+			transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 		}
 	}
 }
